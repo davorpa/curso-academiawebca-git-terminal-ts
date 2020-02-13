a0058c0c0cb2321e28b50e00a9211dec0289ba39
davorpa <katelme@gmail.com>
2020-02-13 13:25:54 +0100

    Declarar variables de tipo número

---
+++ 2-variables/number.ts
@@ -0,0 +1,5 @@
+
+// Todos los números en TypeScript son flotantes
+let mi_edad : number = 23;
+let mi_nota : number = 16.55;
+
---
+++ build/2-variables/number.js
@@ -0,0 +1,5 @@
+"use strict";
+// Todos los números en TypeScript son flotantes
+var mi_edad = 23;
+var mi_nota = 16.55;
+//# sourceMappingURL=number.js.map
