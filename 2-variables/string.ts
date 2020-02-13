58178de51cc89a5d248126e432dc57225c7283ae
davorpa <katelme@gmail.com>
2020-02-13 14:21:27 +0100

    Definición de variables de tipo string

---
+++ 2-variables/string.ts
@@ -0,0 +1,19 @@
+// Comillas dobles
+let pelicula : string = "Misión Imposible";
+// Comillas simples
+let serie : string = 'Hannibal';
+// Comillas invertidas (template strings)
+let poema : string = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna 
+aliqua. Feugiat in ante metus dictum at tempor commodo. 
+Tincidunt praesent semper feugiat nibh sed pulvinar. 
+Laoreet sit amet cursus sit. Volutpat blandit aliquam etiam 
+erat velit scelerisque in dictum. 
+In massa tempor nec feugiat nisl pretium fusce id. 
+Nulla facilisi cras fermentum odio eu feugiat pretium. 
+Pellentesque nec nam aliquam sem et tortor consequat. 
+Euismod quis viverra nibh cras. 
+Feugiat scelerisque varius morbi enim. 
+Pharetra diam sit amet nisl suscipit adipiscing bibendum est.
+`;
---
+++ build/2-variables/string.js
@@ -0,0 +1,8 @@
+"use strict";
+// Comillas dobles
+var pelicula = "Misión Imposible";
+// Comillas simples
+var serie = 'Hannibal';
+// Comillas invertidas (template strings)
+var poema = "\nLorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore et dolore magna \naliqua. Feugiat in ante metus dictum at tempor commodo. \nTincidunt praesent semper feugiat nibh sed pulvinar. \nLaoreet sit amet cursus sit. Volutpat blandit aliquam etiam \nerat velit scelerisque in dictum. \nIn massa tempor nec feugiat nisl pretium fusce id. \nNulla facilisi cras fermentum odio eu feugiat pretium. \nPellentesque nec nam aliquam sem et tortor consequat. \nEuismod quis viverra nibh cras. \nFeugiat scelerisque varius morbi enim. \nPharetra diam sit amet nisl suscipit adipiscing bibendum est.\n";
+//# sourceMappingURL=string.js.map
