81666344e404c54e7372507c437037439b8d4ce6
davorpa <katelme@gmail.com>
2020-02-13 13:09:51 +0100

    Cambio valor tipado variable booleana ERROR

--- 2-variables/boolean.ts
+++ 2-variables/boolean.ts
@@ -1,4 +1,4 @@
 let soy_alto : boolean = true;
 let soy_fan : boolean = false;
 
-soy_alto = 20;
+//soy_alto = 20; //ERROR typescript por ser lenguaje fuertemente tipado
--- build/2-variables/boolean.js
+++ build/2-variables/boolean.js
@@ -1,5 +1,5 @@
 "use strict";
 var soy_alto = true;
 var soy_fan = false;
-soy_alto = 20;
+//soy_alto = 20; //ERROR typescript por ser lenguaje fuertemente tipado
 //# sourceMappingURL=boolean.js.map
