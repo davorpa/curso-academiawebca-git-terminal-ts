003da5cf424ca2527302585afaeb5ba38d1dd3b0
davorpa <katelme@gmail.com>
2020-02-13 12:47:51 +0100

    Declarar e Inicializar variables

--- 2-variables/variables.ts
+++ 2-variables/variables.ts
@@ -3,3 +3,6 @@ let nombre_de_mi_variable;
 
 // Inicializar
 nombre_de_mi_variable = true;
+
+// Declara e Inicializa
+let equipo = "Real Madrid";
--- build/2-variables/variables.js
+++ build/2-variables/variables.js
@@ -3,4 +3,6 @@
 var nombre_de_mi_variable;
 // Inicializar
 nombre_de_mi_variable = true;
+// Declara e Inicializa
+var equipo = "Real Madrid";
 //# sourceMappingURL=variables.js.map
