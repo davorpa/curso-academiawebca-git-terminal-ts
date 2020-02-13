5ac15f59d56db3fd2a28809382582a835599a6fa
davorpa <katelme@gmail.com>
2020-02-13 12:45:40 +0100

    tsc -w Hola Mundo

---
+++ build/1-holamundo/holamundo.js
@@ -0,0 +1,9 @@
+"use strict";
+function saludar(nombre) {
+    return "Hola " + nombre;
+}
+var mi_nombre = "David Ordás";
+var saludo = saludar(mi_nombre);
+console.log(mi_nombre);
+console.log(saludo);
+//# sourceMappingURL=holamundo.js.map
